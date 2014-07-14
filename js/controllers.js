2aee109a1425d417d340a66f20b8acf531b1dfc0
Motoharu ONO <onomotoharu@gmail.com>
2014-07-15 04:47:24 +0900

    card drug implemtend

--- js/controllers.js
+++ js/controllers.js
@@ -3,4 +3,13 @@ var cheeseControllers = angular.module('cheeseControllers',[]);
 cheeseControllers.controller('RecommendCtrl',function($scope,$http,$rootScope, $routeParams){
   $rootScope.headerShow = true;
   $rootScope.footerShow = false;
+
+$(window).on('touchmove.noScroll', function(e) {
+    e.preventDefault();
+});
+
+// function(){
+	$(".recommend_card").draggable();
+// };
+
 });
